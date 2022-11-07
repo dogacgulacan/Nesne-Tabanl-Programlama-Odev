3261ceb0a867284e5cf2d001783c8d91b6fcff76
dogacgulacan <99399656+dogacgulacan@users.noreply.github.com>
2022-11-07 23:57:07 +0300

    Add files via upload

---
+++ Lab1Viewer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1viewer;
+import javax.swing.JFrame;
+import java.io.*;
+
+
+public class Lab1Viewer {
+
+    
+   
+    public static void main(String[] args) {
+      JFrame frame = new Lab1Frame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+    }
+    
+}
